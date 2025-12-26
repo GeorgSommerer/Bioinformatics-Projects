bceb2b922560c2ab3543eabab15a8ef8a005d209
GeorgSommerer <gc.sommerer@gmail.com>
2025-12-26 15:16:07 +0100

    Asymptotic analysis plot added

--- diff_eq_models/discrete_main.py
+++ diff_eq_models/discrete_main.py
@@ -1,17 +1,19 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import math
 from discrete_models import *
 
-def plot_np(res,timesteps,v):
+def plot_main(res,timesteps,v,plot_which):
     minval = np.min([0,np.min(res)])-0.1*np.abs(np.min([0,np.min(res)]))
-    maxval = np.max([1,np.max(res)])+0.1*np.abs(np.max([1,np.max(res)]))   
-    plot_timesteps(res,timesteps,v,minval,maxval)
-    if (len(v.inits_names)>1):
-        plot_against(res,timesteps,v,minval,maxval)   
-    else:
+    maxval = np.max([1,np.max(res)])+0.1*np.abs(np.max([1,np.max(res)]))  
+
+    if (plot_which & 1 << 0):
+        plot_timesteps(res,timesteps,v,minval,maxval)
+    if (plot_which & 1 << 1):
+        plot_phase(res,timesteps,v,minval,maxval)   
+    if (plot_which & 1 << 2):
         plot_cobweb(res,timesteps,v,minval,maxval)  
+    if (plot_which & 1 << 3):
+        plot_asymptotic(res,timesteps,v,minval,maxval)  
+        
     plt.show() 
 
 """
@@ -22,41 +24,36 @@ def plot_timesteps(res,timesteps,v,minval,maxval):
     for i_param in range(len(v.params_vec)):
         for j_init in range(len(v.inits_vec)):
             for m_var in range(len(v.inits_names)):
-                if i_param == len(v.params_vec)-1:
-                    axs[j_init,i_param].plot(timesteps,res[i_param,j_init,:,m_var],label=f"{v.inits_names[m_var]}={v.inits_vec[j_init][m_var]}")
-                    axs[j_init,i_param].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-                else:
-                    axs[j_init,i_param].plot(timesteps,res[i_param,j_init,:,m_var])
+                axs[j_init,i_param].plot(timesteps,res[i_param,j_init,:,m_var],label=f"{v.inits_names[m_var]}0={v.inits_vec[j_init][m_var]}")
+                    
+                axs[j_init,len(v.params_vec)-1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
                 axs[j_init,i_param].set_ylim([minval,maxval])
-            axs[0,i_param].set_title(",".join([f"{v.params_names[l]}={v.params_vec[i_param][l]}" for l in range(len(v.params_names))]))   
+        axs[0,i_param].set_title(",".join([f"{v.params_names[l]}={v.params_vec[i_param][l]}" for l in range(len(v.params_names))]))   
 
 """
 For each set of parameters and each pair of variables, a phase diagram for all initial values is created.
 Maybe add phase space: mvals, nvals = meshgrid(np.linspace) -> mdot, ndot = f(mvals, nvals), g(mvals, nvals) -> streamplot(mvals, nvals, mdot, ndot)
 """
-def plot_against(res,timesteps,v,minval,maxval):
+def plot_phase(res,timesteps,v,minval,maxval):
     fig, axs = plt.subplots(math.comb(len(v.inits_names),2),len(v.params_vec),squeeze=False)
     for i_param in range(len(v.params_vec)):
         for j_init in range(len(v.inits_vec)):
             combos = 0
             for m_var in range(len(v.inits_names)):
                 for n_var in range(m_var+1,len(v.inits_names)):
-                    if i_param == len(v.params_vec)-1:
-                        axs[combos,i_param].plot(res[i_param,j_init,:,m_var],res[i_param,j_init,:,n_var],label=f"{v.inits_names[m_var]}={v.inits_vec[j_init][m_var]}\n{v.inits_names[n_var]}={v.inits_vec[j_init][n_var]}")
-                        axs[combos,i_param].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-                    else:
-                        axs[combos,i_param].plot(res[i_param,j_init,:,m_var],res[i_param,j_init,:,n_var])                
+                    axs[combos,i_param].plot(res[i_param,j_init,:,m_var],res[i_param,j_init,:,n_var],label=f"{v.inits_names[m_var]}0={v.inits_vec[j_init][m_var]}\n{v.inits_names[n_var]}0={v.inits_vec[j_init][n_var]}")
+        
+                    axs[combos,len(v.params_vec)-1].legend(loc='center left', bbox_to_anchor=(1, 0.5))                
                     axs[combos,i_param].set_xlabel(v.inits_names[m_var])
                     axs[combos,i_param].set_ylabel(v.inits_names[n_var])
                     axs[combos,i_param].set_xlim([minval,maxval])
                     axs[combos,i_param].set_ylim([minval,maxval])
                     combos += 1
 
-            axs[0,i_param].set_title(",".join([f"{v.params_names[l]}={v.params_vec[i_param][l]}" for l in range(len(v.params_names))])) 
+        axs[0,i_param].set_title(",".join([f"{v.params_names[l]}={v.params_vec[i_param][l]}" for l in range(len(v.params_names))])) 
 
 """
 If only 1 variable exists, a cobweb plot is created for each pair of parameters and initial values.
-Plot the behavior according to 
 """
 def plot_cobweb(res,timesteps,v,minval,maxval):
     fig, axs = plt.subplots(len(v.inits_vec),len(v.params_vec),squeeze=False)
@@ -73,39 +70,56 @@ def plot_cobweb(res,timesteps,v,minval,maxval):
                 vertical.append(res[i_param,j_init,t,0])
                 horizontal.append(res[i_param,j_init,t,0])
                 vertical.append(res[i_param,j_init,t,0])
-            if i_param == len(v.params_vec)-1:
-                axs[j_init,i_param].plot(horizontal,vertical,'b',label=f"{v.inits_names[0]}={v.inits_vec[j_init][0]}")
-                axs[j_init,i_param].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-            else:
-                axs[j_init,i_param].plot(horizontal,vertical,'b')
+            axs[j_init,i_param].plot(horizontal,vertical,label=f"{v.inits_names[0]}0={v.inits_vec[j_init][0]}")
 
+            axs[j_init,len(v.params_vec)-1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
             axs[j_init,i_param].set_xlabel(f"{v.inits_names[0]}(n)")
-            if i_param == 0:
-                axs[j_init,i_param].set_ylabel(f"{v.inits_names[0]}(n+1)")
+            axs[j_init,0].set_ylabel(f"{v.inits_names[0]}(n+1)")
             axs[j_init,i_param].set_xlim([minval,maxval])
             axs[j_init,i_param].set_ylim([minval,maxval])
-            axs[0,i_param].set_title(",".join([f"{v.params_names[l]}={v.params_vec[i_param][l]}" for l in range(len(v.params_names))])) 
+        axs[0,i_param].set_title(",".join([f"{v.params_names[l]}={v.params_vec[i_param][l]}" for l in range(len(v.params_names))])) 
+
+"""
+Plots the asymptotic behavior of the model for every parameter across all variations of that parameter for all initial values
+"""
+def plot_asymptotic(res,timesteps,v,minval,maxval):
+    fig, axs = plt.subplots(len(v.inits_vec),len(v.params_names),squeeze=False)
+    for i_parname in range(len(v.params_names)):
+        for j_init in range(len(v.inits_vec)):
+            for k_param in range(len(v.params_vec)):
+                for t in range(int(len(timesteps)/2),len(timesteps)):
+                    axs[j_init,i_parname].plot(v.params_vec[k_param][i_parname],res[k_param,j_init,t,0],'b.',alpha=0.3)
+
+            axs[j_init,i_parname].set_xlabel(v.params_names[i_parname])
+            axs[j_init,0].set_ylabel(f"{v.inits_names[0]}(eq)")
+            axs[j_init,i_parname].set_ylim([minval,maxval])
+                
         
 def main():
     """
     models[i][0] is the name of the model, as has to be given as input to Set_Model
     models[i][1] is the number of timesteps n
     models[i][2] is the step size h; therefore, n*h is the total time passed
+    models[i][3] is the plotting behavior: Position 0 stands for plot_timesteps, 1 for plot_phase, 2 for plot_cobweb, and 3 for plot_asymptotic
     """
-    models = [("PDE_2",30,1),
-        ("Logistic",400,1),
-        ("LV_Euler",5000,0.01),
-        ("LV_PPM",5000,0.01),
-        ("LV_PPM_3",5000,0.01),
-        ("van_del_Pol",10000,0.01),
-        ("Bifurcation",30,1)]
+    models = [
+        ("Logistic",[400],[1],[0b1101]),
+        ("Bifurcation",[200,30],[1,1],[0b1000,0b0101]), #0: bifurcation, 1: cobweb
+        ("Logistic_Map",[100,30],[1,1],[0b1000,0b0101]), #0: bifurcation, 1: cobweb
+        ("PDE_2",[30],[1],[0b0011]),
+        ("LV_Euler",[5000],[0.01],[0b0011]),
+        ("LV_PPM",[5000,50000],[0.01,0.01],[0b0011,0b0010]), # 0: standard analysis, 1: test for chaotic behavior
+        ("LV_PPM_3",[5000],[0.01],[0b0011]),
+        ("van_del_Pol",[10000],[0.01],[0b0011])
+        ]
     chosen_model = 2
+    chosen_params = 1
 
-    n = models[chosen_model][1]
-    h = models[chosen_model][2]
+    n = models[chosen_model][1][chosen_params]
+    h = models[chosen_model][2][chosen_params]
     timesteps = np.linspace(0,n*h,n)
 
-    v = Set_Model(models[chosen_model][0],h)
+    v = Set_Model(models[chosen_model][0],chosen_params,h)
     #For every pair (i,j) of parameters and initial values the model is iterated over, res_matrix contains the output for all variables throughout all timesteps
     res_matrix = np.empty([len(v.params_vec),len(v.inits_vec),len(timesteps),len(v.inits_names)])
     for i in range(len(v.params_vec)):
@@ -115,7 +129,7 @@ def main():
                 mymodel.update()
                 mymodel.observe()
             res_matrix[i,j,:,:] = mymodel.result
-    plot_np(res_matrix,timesteps,v)
+    plot_main(res_matrix,timesteps,v,models[chosen_model][3][chosen_params])
 
 if __name__ == "__main__":
     main()
--- diff_eq_models/discrete_models.py
+++ diff_eq_models/discrete_models.py
@@ -7,6 +7,7 @@ from itertools import product
 """
     Chooses the type of model as well as the parameters and initial values on which it is to be run
     :param str model: A string containing the model name, which can be obtained from the models array in the main method
+    :param int which: Given multiple sets of parameters/initial values (for different tasks, e.g. cobweb plot vs. bifurcation plot), choose which one to use
     :param float h: The step size which is used in the model.
     :param array(tuple) params_vec: Every tuple contains a set of parameters to try (tuple size has to match the length of self.params_names)
     :param array(tuple) inits_vec: Every tuple contains a set of initial values to try (tuple size has to match the length of self.inits_names)
@@ -17,49 +18,65 @@ from itertools import product
         The inputs of each function have to be the same and their order has to match the order in self.params_names then self.inits_names
 """
 class Set_Model:
-    def __init__(self,model,h=0.01):
+    def __init__(self,model,which=0,h=0.01):
+        #PDE with 2 variables
+        self.which = which
         if model == "PDE_2":
             self.params_vec = [(np.array([[0.5,1],[-0.5,1]]),np.array([0,0]))]
             self.inits_vec = list(product(np.arange(-2.0,2.0,1.),np.arange(-2.0,2.0,1.)))
             self.params_names = ["A","b"]
-            self.inits_names = ["x0","y0"]
+            self.inits_names = ["x","y"]
             self.functions = [lambda A,b,x,y: np.matmul(A,np.array([x,y]))+b]
+        #Logistic model
         if model == "Logistic":
             self.params_vec = [(0.5,500)]
             self.inits_vec = [(1,),(10,),(50,),(100,),(200,),(500,)] 
             self.params_names = ["r","K"]
-            self.inits_names = ["x0"]
+            self.inits_names = ["x"]
             self.functions = [lambda r,K,x: x+r*x*(1-x/K)]
+        #Euler-discretized Lotka-Volterra model
         if model == "LV_Euler":
             self.params_vec = [(1,1,1,1,h)]
             self.inits_vec = [(0.1,0.1)] 
             self.params_names = ["a","b","c","d","h"]
-            self.inits_names = ["x0","y0"]
+            self.inits_names = ["x","y"]
             self.functions = [lambda a,b,c,d,h,x,y: x+h*(a*x-b*x*y), lambda a,b,c,d,h,x,y: y+h*(c*x*y-d*y)]
+        #Lotka-Volterra model discretized with a positivity preserving map (PPM)
         if model == "LV_PPM":
             self.params_vec = [(1,1,1,2,h)]
-            self.inits_vec = [(0.3,0.2)] 
+            self.inits_vec = [[(0.3,0.2)],list(product(np.arange(0.3,0.30003,0.00001),np.arange(0.2,0.20003,0.00001)))][self.which] #0: standard analysis, 1: test for chaotic behavior
             self.params_names = ["a","b","c","d","h"]
-            self.inits_names = ["x0","y0"]
+            self.inits_names = ["x","y"]
             self.functions = [lambda a,b,c,d,h,x,y: x*(1+a*h)/(1+b*h*y),lambda a,b,c,d,h,x,y: y*(1+c*h*(x*(1+a*h)/(1+b*h*y)))/(1+d*h)]
+        #PPM-discretized Lotka-Volterra model with additional ressource Z that is consumed by X but does not grow 
         if model == "LV_PPM_3":
             self.params_vec = [(1,1,1,1,1,h),(1,1,1,1,0.05,h),(0.1,0.1,0.1,0.1,0.05,h)]
             self.inits_vec = [(10,0.3,0.2)] 
             self.params_names = ["a","b","c","d","e","h"]
-            self.inits_names = ["z0","x0","y0"]
+            self.inits_names = ["z","x","y"]
             self.functions = [lambda a,b,c,d,e,h,z,x,y: z/(1+e*h*x),lambda a,b,c,d,e,h,z,x,y: x*(1+a*h*(z/(1+e*h*x)))/(1+b*h*y),lambda a,b,c,d,e,h,z,x,y: y*(1+c*h*(x*(1+a*h*z/(1+e*h*x))/(1+b*h*y)))/(1+d*h)]
+        #Van-del-Pol oscillator
         if model == "van_del_Pol":
             self.params_vec = [(-1,h),(-0.1,h),(0,h),(.1,h),(1,h)]
             self.inits_vec = [(0.1,0.1),(0.5,0.5),(1,1)] 
             self.params_names = ["r","h"]
-            self.inits_names = ["x0","y0"]
+            self.inits_names = ["x","y"]
             self.functions = [lambda r,h,x,y: x+y*h, lambda r,h,x,y: y+h*(-r*(x**2-1)*y-x)]
+        #A simple model that shows period-doubling bifurcations
         if model == "Bifurcation":
-            self.params_vec = [(0.1,),(0.5,),(1.0,),(1.1,),(1.5,),(1.6,)]
+            self.params_vec = [[(r,) for r in np.arange(0.,2.,0.01)],[(0.1,),(0.5,),(1.0,),(1.1,),(1.5,),(1.6,)]][self.which] #0: bifurcation, 1: Cobweb
             self.inits_vec = [(0.1,)]
             self.params_names = ["r"]
-            self.inits_names = ["x0"]
+            self.inits_names = ["x"]
             self.functions = [lambda r,x: x+r-x**2]
+        #A simplified version of the Logistic function used for bifurcation analysis
+        if model == "Logistic_Map":
+            self.params_vec = [[(r,) for r in np.arange(0,4,0.05)],[(r,) for r in range(5)]][self.which] #0: bifurcation, 1: Cobweb
+            self.inits_vec = [(0.1,),(0.3,),(0.51,)]
+            self.params_names = ["r"]
+            self.inits_names = ["x"]
+            self.functions = [lambda r,x: r*x*(1-x)]
+
 
 """
     For any set of parameters and initial values, calculates the outcome of the discretized equations of the model at the next step
